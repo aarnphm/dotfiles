54fdc244b26f4c3bad432db595db44520bfd94fe
aar0npham <aaronpham0103@gmail.com>
2020-06-02 21:12:55 -0400

    preload completion instead of loading in zshrc

--- zsh/.zsh/link.zsh
+++ zsh/.zsh/link.zsh
@@ -13,3 +13,15 @@ if [ -d /mnt/Centralized/pictures -a -f $HOME/Pictures/saved ]; then
 	done;
 fi;
 unset picture;
+
+# check link for zpreztorc
+if [ ! -f $HOME/.zpreztorc ]; then
+	echo "requires zpreztorc, checking whether installed prezto or not..."
+	if [ ! -d $HOME/.zprezto ]; then
+		echo "prezto is not installed, should be already tho"
+		exit
+	fi
+	for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+		ln "$rcfile" "$HOME/dotfiles/zsh/.${rcfile:t}"
+	done;
+fi;
