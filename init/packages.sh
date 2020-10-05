a52909b329af2a669ec6867d1e0468a739499edb
aar0npham <aaronpham0103@gmail.com>
2020-10-05 11:43:29 -0400

    updated

--- init/packages.sh
+++ init/packages.sh
@@ -8,31 +8,25 @@ export PKG=(
 	awesome xmonad xmobar xmonad-contrib
 	feh nitrogen
 	alacritty termite xdg-user-dirs
-	firefox
+	firefox nm-connection-editor
 	pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils
 	cadence gcc cmake ripgrep ctags
-	obs-studio redshift vlc
-	xsettingsd xsecurelock
-	imagemagick acpi neovim ibus ibus-unikey
-	gnome-keyring libgnome-keyring
+	obs-studio redshift vlc xsettingsd xsecurelock
+	imagemagick acpi neovim ibus ibus-unikey gnome-keyring libgnome-keyring
 	bluez blueman nnn bluez-utils pavucontrol
-	scrot cmatrix xorg xorg-xinit lxappearance lxtask xfce4-power-manager pcmanfm
-	docker zathura curl
+	scrot docker zathura curl
 )
 export AUR=(
-	picom-git ncdu bleachbit cacheclean nm-connection-editor
+	picom-git ncdu bleachbit cacheclean 
 	adobe-source-han-serif-otc-fonts adobe-source-han-sans-otc-fonts noto-fonts-emoji
 	discord papirus-icon-theme
-	dmenu-git
-	github-cli rofi-git twmn-git
+	dmenu-git github-cli rofi-git twmn-git
 	visual-studio-code-bin
 	autofs fet.sh-git polkit dex inkscape
-	mpd ncmpcpp-git wine winetricks lib32-gnutls samba
+	wine winetricks lib32-gnutls samba
 	gyazo zoom postgresql
 	pgadmin4 spicetify-cli
-	materia-gtk-theme
-	spotify breeze-default-cursor-theme
+	materia-gtk-theme breeze-default-cursor-theme
 	vmware-workstation
-	nodejs npm
-	file-roller rainbow
+	nodejs npm rainbow
 )
