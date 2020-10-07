5b01268eb16201d8d84f1051fa4f8aa69bf2b0d9
aar0npham <aaronpham0103@gmail.com>
2020-10-06 21:27:42 -0400

    updated fstab

--- init/packages.sh
+++ init/packages.sh
@@ -2,9 +2,7 @@
 
 export PKG=(
 	mesa stow tmux arandr dialog
-	realtime-privileges
-	awesome xmonad xmobar xmonad-contrib
-	feh nitrogen tree
+	awesome	feh nitrogen tree
 	alacritty termite xdg-user-dirs
 	firefox nm-connection-editor
 	pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils
@@ -17,8 +15,7 @@ export PKG=(
 export AUR=(
 	ncdu bleachbit cacheclean kdocker spotify
 	adobe-source-han-serif-otc-fonts adobe-source-han-sans-otc-fonts noto-fonts-emoji
-	discord papirus-icon-theme
-	dmenu-git github-cli slack-desktop
+	discord papirus-icon-theme dmenu-git github-cli slack-desktop
 	visual-studio-code-bin autofs fet.sh-git polkit dex inkscape
 	wine winetricks lib32-gnutls samba
 	gyazo zoom postgresql pgadmin4 spicetify-cli
