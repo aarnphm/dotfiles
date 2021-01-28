6dd99a9f3ff7730d4c55d4ca55cff65cf8b9d8c7
aarnphm <aaronpham0103@gmail.com>
2021-01-29 00:11:57 +0700

    Fri:01-29:00:11: cron updates

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -3,8 +3,11 @@
 # get datetime
 datetime=$(date +'%a:%m-%d:%R')
 
+# copy lightdm configs to scripts
+LIGHTDM_DIR=/etc/lightdm
+cp $LIGHTDM_DIR/lightdm.conf $LIGHTDM_DIR/lightdm-mini-greeter.conf $HOME/dotfiles/scripts
+
 # execute scripts
-HOME=/home/aarnphm
 cd $HOME/dotfiles && git add .
 git commit -am "$datetime: cron updates"
 git push --all
