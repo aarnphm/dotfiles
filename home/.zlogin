4bb3e527c028673cb74222cbb6ea0d0eede596d4
aarnphm <aaronpham0103@gmail.com>
2021-02-02 02:12:56 +0700

    Tue:02-02:02:12: cron updates

--- home/.zlogin
+++ home/.zlogin
@@ -13,13 +13,13 @@
         zrecompile -pq "$zcompdump"
     fi
     # zcompile .zshrc
-    zrecompile -pq ${ZDOTDIR:-${HOME}}/.zshrc
-    zrecompile -pq ${ZDOTDIR:-${HOME}}/.zprofile
-    zrecompile -pq ${ZDOTDIR:-${HOME}}/.zshenv
+    # zrecompile -pq ${ZDOTDIR:-${HOME}}/.zshrc
+    # zrecompile -pq ${ZDOTDIR:-${HOME}}/.zprofile
+    # zrecompile -pq ${ZDOTDIR:-${HOME}}/.zshenv
     # recompile all zsh or sh
-    for f in $ZSHCONFIG/**/*.*sh
-    do
-        zrecompile -pq $f
-    done
+    # for f in $ZSHCONFIG/**/*.*sh
+    # do
+    #     zrecompile -pq $f
+    # done
 ) &!
 
--- init/init.sh
+++ init/init.sh
@@ -6,28 +6,25 @@
 # install essentials
 _update system
 
-# init cron
+# log cron
 crontab $HOME/dotfiles/scripts/cronfile
 
 if [[ ! -f $HOME/.vim/autoload/plug.vim ]]; then
     echo_info "Installing vim-plug..."
-    eval $(curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim);
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
 fi
 
 # zinit
 if [[ ! -d $HOME/.zinit ]]; then
     echo_info "Installing zinit..."
-    eval $(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh);
+    curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh;
 fi;
 
 # tpm
 if [[ ! -d $HOME/.tmux/plugins/tpm ]]; then
     echo_info "Install tpm..."
-    eval $(git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm);
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
 fi;
-# install starship if wanted
-# curl -fsSL https://starship.rs/install.sh | bash
 
 # pyenv
 if [[ ! -d $HOME/.pyenv ]]; then
@@ -38,14 +35,16 @@ fi;
 # echo_info "Install poetry..."
 # eval $(curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python);
 
+# nnn plugins install
+echo_info "Install nnn plugins..."
+curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh;
+git clone https://github.com/sebastiencs/icons-in-terminal.git && cd icons-in-terminal && ./install.sh
+
+
 # gcp
 if [[ ! -d $HOME/google-cloud-sdk ]]; then
     echo_info "Install gcloud..."
-    eval $(curl https://sdk.cloud.google.com | bash && exec -l $SHELL);
+    curl https://sdk.cloud.google.com | bash && exec -l $SHELL;
 fi;
 # remember to run gcloud init
 
-# nnn plugins install
-echo_info "Install nnn plugins..."
-eval $(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh);
-eval $(git clone https://github.com/sebastiencs/icons-in-terminal.git && cd icons-in-terminal && ./install.sh)
