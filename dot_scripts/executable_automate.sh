8342951ef0fefecc095ee012632154da9835fe3e
aarnphm <aaronpham0103@gmail.com>
2021-02-03 20:32:23 +0700

    updates

--- dot_scripts/executable_automate.sh
+++ dot_scripts/executable_automate.sh
@@ -1,9 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+set -e
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
 
 # get datetime
 datetime=$(date +'%a:%m-%d:%R')
 
-cd $HOME/dotfiles 
+chezmoi cd
 
 # Check if upstream, else not pull
 if [ $(git rev-parse @)==$(git merge-base @ ${1:-'@{u}'}) ]; then
@@ -11,5 +28,5 @@ if [ $(git rev-parse @)==$(git merge-base @ ${1:-'@{u}'}) ]; then
 fi
 
 # execute scripts
-git add .&& git commit -am "$datetime: cron updates" && git push --all
+git add .&& git commit -am "$datetime: cron chores" && git push --all
 
