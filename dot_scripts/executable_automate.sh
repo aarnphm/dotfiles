61a1b273dd8a09d16ee98324726edb929fba7fe8
aarnphm <aaronpham0103@gmail.com>
2021-02-04 00:59:51 +0700

    Thu:02-04:00:59: cron chores

--- dot_scripts/executable_automate.sh
+++ dot_scripts/executable_automate.sh
@@ -2,26 +2,10 @@
 
 set -e
 
-if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  chezmoi="$bin_dir/chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
-else
-  chezmoi=chezmoi
-fi
-
 # get datetime
 datetime=$(date +'%a:%m-%d:%R')
 
-chezmoi cd
-
+cd $HOME/.local/share/chezmoi
 # Check if upstream, else not pull
 if [ $(git rev-parse @)==$(git merge-base @ ${1:-'@{u}'}) ]; then
     git pull --recurse-submodules
