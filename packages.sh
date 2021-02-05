c804a80b4401abf00c925ba2606ed8f97db02aba
aarnphm <aaronpham0103@gmail.com>
2021-02-05 09:53:55 +0700

    migrating to chezmoi

--- packages.sh
+++ packages.sh
@@ -9,8 +9,7 @@ awesome	feh nitrogen tree base-devel
 asciinema kitty xdg-user-dirs
 firefox nm-connection-editor pcmanfm
 pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils
-gcc cmake ripgrep ctags lxappearance
-obs-studio redshift vlc xsettingsd xsecurelock
+gcc cmake ripgrep ctags lxappearance redshift vlc xsettingsd xsecurelock
 imagemagick acpi ibus ibus-unikey gnome-keyring libgnome-keyring
 bluez blueman nnn bluez-utils pavucontrol
 scrot docker zathura curl vim cronie neovim caprine termite
@@ -18,12 +17,13 @@ lightdm gparted mate-polkit qt5-base htop
 kvantum-qt5 wireless-tools network-manager-applet
 openssh openvpn sshfs net-tools
 )
+
 export AUR=(
-awmtt rofi lightdm-mini-greeter nm-applet ncdu bleachbit cacheclean kdocker noip
+awmtt rofi bitwarden lightdm-mini-greeter nm-applet ncdu bleachbit cacheclean kdocker
 adobe-source-han-serif-otc-fonts adobe-source-han-sans-otc-fonts noto-fonts-emoji
-discord dmenu-git github-cli slack-desktop dnsutils
-visual-studio-code-bin autofs fet.sh-git polkit inkscape samba ccls wine
-jre-openjdk jdk-openjdk zoom postgresql pgadmin4 spicetify-cli 
+discord dmenu-git github-cli slack-desktop dnsutils visual-studio-code-bin
+autofs fet.sh-git polkit inkscape samba ccls wine
+jre-openjdk jdk-openjdk zoom postgresql pgadmin4 spicetify-cli
 materia-gtk-theme papirus-icon-theme qt5ct pyenv odoo
 vmware-workstation nodejs npm rainbow ruby-colorls spotify
 picom-git redshift-qt unclutter optimus-manager-qt bbswitch numix-circle-icon-theme-git
