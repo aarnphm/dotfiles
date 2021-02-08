83cf1b37b1490eece124d1f0e6a2bf39d7bf4e6a
aarnphm <aaronpham0103@gmail.com>
2021-02-08 11:16:58 +0700

    added another way to profile zsh startuptime

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -470,21 +470,6 @@ awful.screen.connect_for_each_screen(
         end
         )
 
-    -- No borders when rearranging only 1 non-floating or maximized client
-    screen.connect_signal(
-        "arrange",
-        function(s)
-            local only_one = #s.tiled_clients == 1
-            for _, c in pairs(s.clients) do
-                if only_one and not c.floating or c.maximized then
-                    c.border_width = 0
-                else
-                    c.border_width = beautiful.border_width
-                end
-            end
-        end
-        )
-
     -- Signal function to execute when a new client appears.
     client.connect_signal(
         "manage",
