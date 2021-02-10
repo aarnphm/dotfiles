80a9d2fade42262a3e378eaa9b4710c4f84c1209
staff <aaronpham0103@gmail.com>
2021-02-10 14:18:12 +0700

    update bootraps script

--- bootstrap/run_once_0001_install_chezmoi.sh
+++ bootstrap/run_once_0001_install_chezmoi.sh
@@ -18,7 +18,7 @@ else
     echo "Chezmoi exists, skipping."
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec chezmoi init --apply "--source=$script_dir"
+# # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+# script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# # exec: replace current process with chezmoi init
+# exec chezmoi init --apply "--source=$script_dir"
