a767bbce279f1a8ced56bd47a87ef3381e1ac2fa
Aaron Pham <aaronpham0103@gmail.com>
2021-08-14 00:00:15 +0700

    [cron] 08/14 00:00AM: chores

---
+++ dot_screenlayout/executable_triple-vertical.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0.8 --mode 2560x1440 --pos 2560x0 --rotate right --output DP-0.1 --mode 1920x1080 --pos 640x40 --rotate normal --output HDMI-0 --primary --mode 2560x1440 --pos 0x1120 --rotate normal --output DP-0 --off --output DP-1 --off --output eDP-1-1 --off --output DP-1-1 --off
