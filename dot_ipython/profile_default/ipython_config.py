da8f46cdb0c49d5d4a9386e41f0cf635fe1ab292
Aaron Pham <aaronpham0103@gmail.com>
2021-08-19 00:00:15 +0700

    [cron] 08/19 00:00AM: chores

--- dot_ipython/profile_default/ipython_config.py
+++ dot_ipython/profile_default/ipython_config.py
@@ -32,7 +32,7 @@
 
 ## lines of code to run at IPython startup.
 #  Default: []
-# c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 
 ## A list of dotted module names of IPython extensions to load.
 #  Default: []
