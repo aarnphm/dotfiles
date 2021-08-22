c192c3f0fdbdf2b819b4c0e5219641a6200c79f4
Aaron Pham <aaronpham0103@gmail.com>
2021-08-23 00:00:30 +0700

    [cron] 08/23 00:00AM: chores

--- dot_screenlayout/executable_dual.sh
+++ dot_screenlayout/executable_dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --off --output DP1-1-1 --off --output DP1-1-1-1 --off --output DP1-1-1-8 --mode 2560x1440 --pos 2560x0 --rotate right --output DP1-1-8 --primary --mode 2560x1440 --pos 0x560 --rotate normal --output VIRTUAL1 --off --output HDMI-1-0 --off --output DP-1-0 --off --output DP-1-1 --off
+xrandr --output DP-0.8 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-0.1 --off --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output eDP-1-1 --off --output DP-1-1 --off
