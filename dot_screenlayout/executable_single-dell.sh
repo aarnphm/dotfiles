7794b71bc2a45fe24388d6764963f5c92efd5dfb
Aaron Pham <aaronpham0103@gmail.com>
2021-09-09 08:35:15 -0400

    feat: single dell

--- dot_screenlayout/executable_single-dell.sh
+++ dot_screenlayout/executable_single-dell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0.8 --off --output DP-0.1 --off --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output eDP-1-1 --off --output DP-1-1 --off
+xrandr --output DP-0.8 --off --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output eDP-1-1 --off --output DP-1-1 --off
